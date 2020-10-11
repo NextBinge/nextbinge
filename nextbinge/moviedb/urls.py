3fa6ecbdf7516649f60a472625a278d2d09b1b04
pranav567 <pranav.nair@spit.ac.in>
2020-10-11 12:53:52 +0530

    movie page queries views

--- nextbinge/moviedb/urls.py
+++ nextbinge/moviedb/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('toprated/', views.toprated_view, name='toprated'),
     path('mostpopular/', views.mostpopular_view, name='mostpopular'),
     path('recent/', views.recent_view, name='recent'),
-    path('top50/', views.top50_view, name='top50'),
+    path('toprated/movie/<name>', views.movie_view, name='tratemovie'),
+    path('mostpopular/movie/<name>', views.movie_view, name='mpopmovie'),
+    path('recent/movie/<name>', views.movie_view, name='recentmovie'),
 ]
